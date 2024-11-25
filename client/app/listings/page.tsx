879e7847836606ef2ccd06fe9cc2400cb7e47248
Evandabest <evanhaque1@gmail.com>
2024-11-25 02:40:54 -0500

    switched to server component and added navigation

--- client/app/listings/page.tsx
+++ client/app/listings/page.tsx
@@ -1,31 +1,41 @@
-"use client"
+"use server"
+import { createClient } from '@/utils/supabase/server';
+import ListingCard from '@/components/ListingCard';
+import Link from 'next/link';
 
-import Listingpage from '@/components/Listingpage';
-import { createClient } from '@/utils/supabase/client';
-import { useRouter } from 'next/navigation';
-import React, { useEffect } from 'react'
-
-const ListingsPage = () => {
-  const router = useRouter();
-  const supabase = createClient();
-
-  useEffect(() => {
-    const checkUser = async() => {
-      const {data: {session} } = await supabase.auth.getSession();
-      
-      if(!session){
-        router.push('/login');
-      };
-    };
-
-    checkUser();
-  }, [router])
-
+const ListingsPage = async () => {
+  const supabase = await createClient();
+  const {data} = await supabase.from("listings").select("*");
+  console.log(data)
+  
   return (
-    <>
-    </>
+    <div className="container mx-auto px-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {data && data?.map((listing: any) => {
+          return (
+            <Link 
+              href={`/listings/${listing.listing_id}`} 
+              key={listing.listing_id}
+              className="w-full cursor-pointer"
+            >
+              <ListingCard
+                imageUrl={listing.listing_photo_id || "/placeholder.jpg"}
+                status={listing.status}
+                address={listing.address}
+                rent={listing.rent}
+                beds={listing.beds}
+                baths={listing.baths}
+                levels={listing.levels}
+                sqft={listing.sqft}
+                author={listing.author_id}
+                
+              />
+            </Link>
+          )
+        })}
+      </div>
+    </div>
   )
 }
 
 export default ListingsPage;
-
