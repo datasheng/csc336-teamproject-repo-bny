1297af5f70e846fc93c02bc6bff7faf7083ce5a0
Evandabest <evanhaque1@gmail.com>
2024-12-18 11:41:58 -0500

    updated api handling requests

--- real-estate-ai/app.py
+++ real-estate-ai/app.py
@@ -2,19 +2,40 @@
 from flask import Flask, request, jsonify
 import pickle
 import numpy as np
+import os
+import joblib
+import pandas as pd
 
 app = Flask(__name__)
 
-# Load the model
-with open('rent-prediction.pkl', 'rb') as file:
-    model = pickle.load(file)
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+model_path = os.path.join(current_dir, 'rent-prediction.pkl')
+model = joblib.load(model_path)
+
 
 @app.route('/predict', methods=['POST'])
-def predict():
+def predict():    
+    #load data from the body
     data = request.get_json()
-    features = np.array(data['features']).reshape(1, -1)
-    prediction = model.predict(features)
-    return jsonify({'predicted_rent': float(prediction[0])})
+    #convert data to dataframe
+
+    new_data = {
+    'type_number': 1,
+    'sqfeet': data['sqfeet'],
+    'lat': data['lat'],    
+    'long': data['long'],
+    'beds': data['beds'],
+    'baths': data['baths'],
+    'state_number': 1,
+    }
+    print(data)
+
+    df = pd.DataFrame([new_data])
+
+    prediction = model.predict(df)
+
+    return jsonify({float(prediction[0])})
 
 if __name__ == '__main__':
     app.run()
